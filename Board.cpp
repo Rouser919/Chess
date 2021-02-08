56d360e3012d859347aecfcf97b711650b93280e
3c.mateuszstopa@gmail.com <3c.mateuszstopa@gmail.com>
2021-02-08 19:06:56 +0100

    Update functions

--- Board.cpp
+++ Board.cpp
@@ -1,28 +1,7 @@
 #include "Board.h"
 #include <iostream>
 #include <cctype>
-/*
-enum class Turn { White, Black };
-class Board
-{
-private:
-	 Piece *table_of_chessboard[8][8];
-	 Turn _actual_turn;
-	 cords _actual, _next_move;
-	 bool is_White_King_in_check_mate;
-	 bool is_Black_King_in_check_mate;
-	 bool promote_pawn(Turn actual);
-	 void init_new_table();
-public:
-	Board();
-	~Board() {};
-	void reset_board();
-	bool get_cords_to_move();
-	void move_piece();
-	void check_for_check_mate(Turn actual);
-};
-*/
-bool Board::promote_pawn(Turn actual) {
+bool Board::can_promote_pawn() {
 	if (table_of_chessboard[_next_move.y][_next_move.x]->_colour == Colour::White && _next_move.y == 7) {
 		return 1;
 	}
@@ -30,6 +9,65 @@ bool Board::promote_pawn(Turn actual) {
 		return 1;
 	}
 	return 0;
+}
+void Board::promote_pawn() {
+	std::cout << "Choose what do you want" << std::endl <<
+		"1. Queen   2.Rook   3.Bishop   4.Knight" << std::endl;
+	int type;
+	std::cin >> type;
+	while (type < 0 || type>4) {
+		std::cout << "Wrong number! Type again" << std::endl;
+		std::cin >> type;
+	}
+	switch (type) {
+	case 1:
+		if (_actual_turn == Turn::White) {
+			delete  table_of_chessboard[_next_move.y][_next_move.x];
+			table_of_chessboard[_next_move.y][_next_move.x] = new Queen(Colour::White, _next_move.y, _next_move.x);
+		}
+		else {
+			delete  table_of_chessboard[_next_move.y][_next_move.x];
+			table_of_chessboard[_next_move.y][_next_move.x] = new Queen(Colour::Black, _next_move.y, _next_move.x);
+
+		}
+		break;
+	case 2:
+		if (_actual_turn == Turn::White) {
+			delete  table_of_chessboard[_next_move.y][_next_move.x];
+			table_of_chessboard[_next_move.y][_next_move.x] = new Rook(Colour::White, _next_move.y, _next_move.x);
+		}
+		else {
+			delete  table_of_chessboard[_next_move.y][_next_move.x];
+			table_of_chessboard[_next_move.y][_next_move.x] = new Rook(Colour::Black, _next_move.y, _next_move.x);
+
+		}
+		break;
+	case 3:
+		if (_actual_turn == Turn::White) {
+			delete  table_of_chessboard[_next_move.y][_next_move.x];
+			table_of_chessboard[_next_move.y][_next_move.x] = new Bishop(Colour::White, _next_move.y, _next_move.x);
+		}
+		else {
+			delete  table_of_chessboard[_next_move.y][_next_move.x];
+			table_of_chessboard[_next_move.y][_next_move.x] = new Bishop(Colour::Black, _next_move.y, _next_move.x);
+
+		}
+		break;
+	case 4:
+		if (_actual_turn == Turn::White) {
+			delete  table_of_chessboard[_next_move.y][_next_move.x];
+			table_of_chessboard[_next_move.y][_next_move.x] = new Knight(Colour::White, _next_move.y, _next_move.x);
+		}
+		else {
+			delete  table_of_chessboard[_next_move.y][_next_move.x];
+			table_of_chessboard[_next_move.y][_next_move.x] = new Knight(Colour::Black, _next_move.y, _next_move.x);
+
+		}
+		break;
+	default:
+		break;
+
+	}
 }
 void Board::init_new_table() {
 	table_of_chessboard[0][0] = new Rook(Colour::White, 0, 0);
@@ -99,6 +137,15 @@ Board::Board() {
 	is_White_King_in_check_mate = false;
 	is_Black_King_in_check_mate = false;
 }
+Board::~Board() {
+	for (auto& i : table_of_chessboard) {
+		for (auto& b : i) {
+			if (b != nullptr) {
+				delete b;
+			}
+		}
+	}
+}
 void Board::reset_board() {
 	for (int y = 0; y < 8; y++) {
 		for (int x = 0; x < 8; x++) {
@@ -159,6 +206,7 @@ bool Board::move_piece() {
 		std::swap(table_of_chessboard[_actual.y][_actual.x], table_of_chessboard[_next_move.y][_next_move.x]);
 		delete table_of_chessboard[_actual.y][_actual.x];
 		table_of_chessboard[_actual.y][_actual.x] = new Piece(Colour::None, _actual.y, _actual.x);
+		if (can_promote_pawn()) promote_pawn();
 		if (_actual_turn == Turn::White) {
 			_actual_turn == Turn::Black;
 		}
@@ -169,12 +217,13 @@ bool Board::move_piece() {
 	return false;
 }
 void Board::check_for_check_mate() {
+	bool tmp_black, tmp_white;
 	for (auto& y : table_of_chessboard) {
 		for (auto& x : y) {
 			if (x->_colour == Colour::White) {
 				for (auto& b : x->_array_of_moves_with_value) {
 					if (std::get<2>(b) > 9500) {
-						is_Black_King_in_check_mate = true;
+						tmp_black = true;
 					}
 
 				}
@@ -183,11 +232,24 @@ void Board::check_for_check_mate() {
 			else if (x->_colour == Colour::Black) {
 				for (auto& b : x->_array_of_moves_with_value) {
 					if (std::get<2>(b) < -9500) {
-						is_White_King_in_check_mate = true;
+						tmp_white = true;
 					}
 
 				}
 
+			}
+		}
+	}
+	if (tmp_black == true) is_Black_King_in_check_mate = true;
+	else is_Black_King_in_check_mate = false;
+	if (tmp_white == true) is_White_King_in_check_mate = true;
+	else is_White_King_in_check_mate = false;
+}
+void Board::update_all_moves() {
+	for (auto& b : table_of_chessboard) {
+		for (auto& i : b) {
+			if (i->getPieceType() != PieceType::None) {
+				i->all_posible_moves_with_value(table_of_chessboard);
 			}
 		}
 	}
--- Board.h
+++ Board.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Piece.h"
+#include <Windows.h>
 enum class Turn {White,Black};
 class Board
 {
@@ -9,14 +10,16 @@ private:
 	 cords _actual, _next_move;
 	 bool is_White_King_in_check_mate;
 	 bool is_Black_King_in_check_mate;
-	 bool promote_pawn(Turn actual);
+	 bool can_promote_pawn();
+	 void promote_pawn();
 	 void init_new_table();
 	 bool valid_cords_actual(char y, char x);
 	 bool valid_cords_next_move(char y, char x);
 	 void check_for_check_mate();
+	 void update_all_moves();
 public:
 	Board();
-	~Board() {};
+	~Board();
 	void reset_board();
 	void get_cords_to_move();
 	bool move_piece();
